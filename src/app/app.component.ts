fb5bb638a0e7f94aa8ad0fd949d6ce9daadfb5d2
oleg.dekhtiarenko <oleg.dekhtiarenko@wonga.com>
2017-01-04 16:04:49 +0200

    123

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,20 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <h1>
+  {{title}}
+</h1>
+<br/>
+<h1>
+   {{test + testS}}
+</h1>
+<app-databinding></app-databinding>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+  title = 'Test test';
+  test = 1;
+  testS = 1;
 }
